5e9c10e76cc426baf0d7fece84e17829c8e69f43
jimncha7 <chacinjimn9@gmail.com>
2025-07-18 14:02:53 -0400

    ola

---
+++ clase pc.py
@@ -0,0 +1,40 @@
+#operadores de personajes
+jimn = 10
+kamila = 15
+resultado = ("jimn"<"kamila") and ("jimn">"kamila")
+print(resultado)
+resultado = not("jimn">"kamila") and ("kamila">"jimn")
+
+#programa de usuario
+
+ 
+ #ejercicios practicos
+ 
+puntuacion = 90
+if puntuacion >= 100:
+     print("obtuviste una b.")
+
+elif puntuacion >= 85:
+    print("obtuviste r.")
+
+
+elif puntuacion >= 87:
+     print("obtuviste u.")
+else: ("que bueno eres mejora mucho mas")
+     
+puntuacion = 80
+if puntuacion >=89:
+    print("obtuviste x c.")
+elif puntuacion >=75:
+     print("obtuviste o.")
+elif puntuacion >=78:
+     print("obtuviste t")
+else: ("estuviste bien mejora")
+
+
+      
+     
+           
+            
+           
+        
